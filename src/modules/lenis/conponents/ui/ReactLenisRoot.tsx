555cc4310d236e5f052b942ddbe70520585f31ef
PrimalCat-Real <romanenko.denis2003@gmail.com>
2026-04-16 18:10:23 +0200

    chore: remove unused code

--- src/modules/lenis/conponents/ui/ReactLenisRoot.tsx
+++ src/modules/lenis/conponents/ui/ReactLenisRoot.tsx
@@ -3,9 +3,7 @@ import { ReactLenis, useLenis } from 'lenis/react'
 import React, { ReactNode } from 'react'
 
 const ReactLenisRoot = ({ children }: { children: ReactNode }) => {
-    const lenis = useLenis((lenis) => {
-        console.log(lenis)
-    })
+
     return (
         <>
             <ReactLenis root />
